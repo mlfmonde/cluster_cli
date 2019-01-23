d3072081e920bcc8dd66e1826848792eeb267423
pyseed <pyseed.dev@gmail.com>
2019-01-23 17:23:56 +0100

    #14 [IMP] consul kb find search pattern

--- cluster/cluster.py
+++ cluster/cluster.py
@@ -12,6 +12,7 @@ from cluster import util
 
 DEFAULT_TIMEOUT = 300
 APP_KEY_SEPARATOR = '.'  # app key prefix/md5 separator
+APP_KV_FIND_PATTERN = 'app/{repo}_{branch}{separator}'
 logger = logging.getLogger(__name__)
 
 
@@ -84,7 +85,7 @@ class Cluster:
         return checks
 
     def get_kv_application(self, repo_name, branch):
-        apps = self.consul.kv.find('app/{repo}_{branch}{separator}'.format(
+        apps = self.consul.kv.find(APP_KV_FIND_PATTERN.format(
                 repo=repo_name,
                 branch=branch,
                 separator=APP_KEY_SEPARATOR
